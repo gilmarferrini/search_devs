43e40e5a7f008d208b8990ab7fc477899b52eb17
ferrinigilmar <ferrinigilmar@gmail.com>
2020-08-31 18:25:00 -0300

    refactoring

---
+++ backend/src/app/controllers/AuthController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import jwt from 'jsonwebtoken';
+import Users from '../models/Users';
+
+const JWT_SECRET = '4584432/*g4f56d4gf*/dg754+gf8d-g*df8g6544';
+
+class AuthController {
+  async login(request: Request, response: Response): Promise<void | Response> {
+    const usersRepository = getRepository(Users);
+    const { username, password } = request.body;
+
+    const checkIfUserExist = await usersRepository.findOne({
+      where: { username },
+    });
+
+    if (!checkIfUserExist) {
+      return response.status(400).json({ message: 'Usuário inexistente' });
+    }
+
+    if (checkIfUserExist) {
+      if (checkIfUserExist.password !== password) {
+        return response.status(401).json({ message: 'Senha incorreta' });
+      }
+    }
+
+    jwt.sign(
+      {
+        id: checkIfUserExist.id,
+        username: checkIfUserExist.username,
+      },
+      JWT_SECRET,
+      { expiresIn: '2h' },
+      (err, token) => {
+        if (err) {
+          return response.status(500).json({ error: 'Erro ao gerar o token' });
+        }
+
+        return response.status(200).json({ token });
+      },
+    );
+  }
+}
+
+export default AuthController;
--- backend/src/routes/users/index.ts
+++ backend/src/routes/users/index.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import UsersController from '../../app/controllers/UsersController';
+import AuthController from '../../app/controllers/AuthController';
 
 const usersRouter = Router();
+
 const usersController = new UsersController();
+const authController = new AuthController();
 
 usersRouter.get('/', usersController.index);
 usersRouter.post('/', usersController.store);
+
+usersRouter.post('/login', authController.login);
 
 export default usersRouter;
